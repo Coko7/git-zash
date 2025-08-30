da21e8f0112ba0297a1b4cda66ec0a57db62f72e
Coko <91132775+Coko7@users.noreply.github.com>
2025-08-30 10:17:56 +0200

    fix: test script broken

--- test.sh
+++ test.sh
@@ -11,12 +11,15 @@ if [ ! -z "$(git stash list)" ]; then
 fi
 
 echo "Hello World" > test1.txt
+git add test1.txt
 git stash push --include-untracked --message "Add test1.txt" -- test1.txt
 
 echo "Bye World" > test2.txt
+git add test2.txt
 git stash push --include-untracked --message "Add test2.txt" -- test2.txt
 
 echo "1 + 1 = 2" > maths.txt
+git add maths.txt
 git stash push --include-untracked --message "Some maths" -- maths.txt
 
 bash git-zash.sh --backup
